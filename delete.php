18d41e9b3dbd847eee7e77e30516ee08d6217f3e
Jonathan Vice <jajvice@gmail.com>
2019-06-24 20:05:22 +0200

    Update delete: use get instead of post

--- delete.php
+++ delete.php
@@ -11,12 +11,12 @@
 		<?php
 		require_once('common.php');
 
-		if (!isset($_POST['id'])) exit();
+		if (!isset($_GET['id'])) exit();
 
 		$helper = new DatabaseHelper();
 
-		$id = $helper->escape($_POST['id']);
-		$delete_item = "DELETE FROM " . ITEMS_TABLE_NAME . "WHERE `id` = '$id'";
+		$id = $helper->escape($_GET['id']);
+		$delete_item = "DELETE FROM " . ITEMS_TABLE_NAME . " WHERE id = '$id'";
 
 		$run = $helper->run_query($delete_item);
 		if ($run === FALSE) die("Failed to delete item: " . $helper->get_last_error());
