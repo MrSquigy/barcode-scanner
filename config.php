fa5996fb56a460b749aff0352165e2d1102e8b5c
Jonathan Vice <jajvice@gmail.com>
2019-06-24 14:07:34 +0200

    Add host config var

--- config.php
+++ config.php
@@ -6,6 +6,7 @@
 	/* MySQL config */
 	define('MYSQL_USERNAME', 'root');
 	define('MYSQL_PASSWORD', '');
+	define('MYSQL_HOST', '127.0.0.1');
 	define('DATABASE_NAME', 'bscan');
 
 	/* MySQL table names */
