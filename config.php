e2a2260d9dacbe8762ce9afe562322fab10d8f0f
Jonathan Vice <jajvice@gmail.com>
2019-06-24 20:17:13 +0200

    Fix comment

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
 	define('MYSQL_HOST', '127.0.0.1');
 	define('DATABASE_NAME', 'bscan');
 
-	/* MySQL table names */
+	/* MySQL table name */
 	define('ITEMS_TABLE_NAME', 'items');
 
 	/* File stuff */
