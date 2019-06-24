8c50f187b34c66f8a85177926171b04ca2ed7223
Jonathan Vice <jajvice@gmail.com>
2019-06-24 19:07:35 +0200

    Update common: add add_item function

--- common.php
+++ common.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('classes/DatabaseHelper.php');
+
 /* Returns item from passed id */
 function get_item($item_id) {
 	$find_item = "SELECT * FROM " . ITEMS_TABLE_NAME . " WHERE id = '$item_id'";
@@ -11,6 +13,15 @@ function get_item($item_id) {
 	$item = $run->fetch_assoc();
 
 	return $item;
+}
+
+function add_item($name, $description, $price, $stock) {
+	$add_item = "INSERT INTO " . ITEMS_TABLE_NAME . " (name, description, price, stock_count) VALUES ('$name', '$description', '$price', '$stock')";
+	$helper = new DatabaseHelper();
+
+	$run = $helper->run_query($add_item);
+	if ($run === FALSE) return "Failed to add item: " . $helper->get_last_error();
+	return "<a href='lookup.php?id=" . $helper->get_last_id() . "'>Successfully added item (ID: " . $helper->get_last_id() . ")</a>";
 }
 
 ?>
