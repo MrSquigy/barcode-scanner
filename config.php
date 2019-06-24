9c5a5ec9feff9be1820c42df032f482700a8ba3f
Jonathan Vice <jajvice@gmail.com>
2019-06-24 13:53:01 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	/*
+	*	This file contains config vars, change everything as required.
+	*/
+
+	/* MySQL config */
+	define('MYSQL_USERNAME', 'root');
+	define('MYSQL_PASSWORD', '');
+	define('DATABASE_NAME', 'bscan');
+
+	/* MySQL table names */
+	define('ITEMS_TABLE_NAME', 'items');
+
+?>
